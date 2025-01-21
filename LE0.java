7954f26008bceeb81d50fbb22b7cd8dac2b31349
221B149 <221B149@juet.co.in>
2025-01-21 12:16:24 +0530

    LE 0

---
+++ LE0.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class juet{
+    String name;
+    int age;
+    void setname(String x){
+    this.name=x;
+    }
+    void setage(int y){
+        this.age=y;
+    }
+    String getname(){
+        return name;
+    }
+    int getage(){
+        return age;
+    }
+}
+class Main{
+public static void main(String args[])  { 
+  juet ob1=new juet();
+  juet ob2=new juet();
+  ob1.setname("divya");
+  ob2.setage(21);
+  System.out.println("Name : "+ ob1.getname());
+  System.out.print("Age : "+ob2.getage());
+  
+} 
+}
